ed6b1cc6da980aaabf540dd60e6786d6d962dc76
Anna Gasparyan <Gasparyan.2003.ag@gmail.com>
2023-01-22 17:32:08 +0400

    Added comments

--- main.cpp
+++ main.cpp
@@ -3,19 +3,22 @@
 
 int main() {
 
+    //inputing the subject name
     char subject[100];
     std::cout << "Input the subject name: ";
     std::cin.getline(subject, 100);
     std::cout << std::endl;
 
+    //creating a Subject object
     Subject s(subject);
 
+    //Getting the name of the subject with GetName() function
     char* subjectName = s.GetName();
 
     std::cout << "Subject name: ";
 
     int i = 0;
-
+    //outputing the name
     while((*(subjectName + i)) != '\0') {
         std::cout << *(subjectName + i);
         ++i;
